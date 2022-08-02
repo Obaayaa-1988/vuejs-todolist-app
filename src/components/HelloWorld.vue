515fb3c18cb18f9322aad4405222cf76c2ec06ea
Abigail <ashbella333@gmail.com>
2022-08-02 17:02:43 +0000

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,76 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div style="border: 1px solid; height: 500px; padding: 5rem">
+    <InputField
+      :placeholder="'enter a letter or number'"
+      :color="'5px solid blue'"
+    />
+    <br />
+    <InputField
+      :placeholder="'Enter your name here'"
+      :color="'5px solid black'"
+    />
+    <br />
+    <InputField
+      :placeholder="'Enter your address here'"
+      :color="'5px solid green'"
+    />
+    <br />
+    <InputField
+      :color="'5px solid red'"
+      :placeholder="'Enter your address here'"
+    />
+    <br />
+    <InputField
+      :placeholder="'Enter your location here'"
+      :color="'5px solid purple'"
+    />
+    <br />
+    <InputField
+      :placeholder="'Enter your job here'"
+      :color="'5px solid yellow'"
+    />
+    <br />
+    <InputField
+      :placeholder="'Enter your country here'"
+      :color="'5px solid pink'"
+    />
+    <br />
+    <InputField
+      :placeholder="'Enter your business here'"
+      :color="'5px solid brown'"
+    />
+    <br />
+    <InputField
+      :placeholder="'Enter your church here'"
+      :color="'5px solid cyan'"
+    />
+    <br />
+    <button id="btn">send</button>
   </div>
 </template>
 
 <script>
+// import InputField from "./components/InputField.vue";
+import InputField from "./InputField.vue";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  components: {
+    InputField,
+  },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#btn {
+  padding: 10px;
+  border: none;
+  background-color: tomato;
+}
 h3 {
   margin: 40px 0 0;
 }
---
+++ src/components/InputField.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input
+      type="text"
+      :placeholder="placeholder"
+      id="letterInput"
+      :style="{
+        border: color,
+        padding: '10px',
+        width: '30%',
+        borderRadius: '10px',
+      }"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputField",
+  props: {
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    color: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style>
+</style>
